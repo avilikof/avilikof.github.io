451a0941543e4487b3f36ba7c6f10ad64585f583
masterblaster <>
2020-11-07 15:22:53 +0000

    add suqrySelector

--- text.js
+++ text.js
@@ -1,15 +1,19 @@
 let HELLO_WOLRD = 'Hello World!!!';
 let ALERT = 'ALERT';
 
-alert(ALERT);
+const myPar = document.querySelector('p');
+const myHeading = document.querySelector('h1');
+
 let userAnswer = prompt('Type your name');
 let result;
 result = confirm(`Is your name ${userAnswer}?`);
 
+myHeading.textContent = HELLO_WOLRD;
+
 if (result) {
-    document.write(`Hello ${userAnswer}`)
+    myPar.textContent = `Hello ${userAnswer}`;
 } else {
-    document.write(HELLO_WOLRD)
+    myPar.textContent = 'I don\' know you.';
 }
 
 // document.write(HELLO_WOLRD + ' From var');
